dacba4a3c0d672bdf8cc2f40f28d301adf807d47
pbplop29 <pbplop@gmail.com>
2023-04-18 18:08:46 +0530

    created the project, used java 17

---
+++ osp/src/main/java/com/lowes3/osp/OspApplication.java
@@ -0,0 +1,13 @@
+package com.lowes3.osp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OspApplication.class, args);
+	}
+
+}
---
+++ osp/src/test/java/com/lowes3/osp/OspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lowes3.osp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
