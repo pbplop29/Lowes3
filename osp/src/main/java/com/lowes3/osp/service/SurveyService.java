83e1d310ce1f3bd46d80ce9a3f4d23f1a4071705
pbplop29 <pbplop@gmail.com>
2023-04-19 00:48:53 +0530

    created and added the methods in service layer

--- osp/src/main/java/com/lowes3/osp/service/SurveyService.java
+++ osp/src/main/java/com/lowes3/osp/service/SurveyService.java
@@ -1,6 +1,14 @@
 package com.lowes3.osp.service;
 
+import com.lowes3.osp.entity.Survey;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface SurveyService {}
+public interface SurveyService {
+    public Survey saveSurvey(Survey survey);
+    public Survey getSurveyById(Integer surveyId);
+    public Survey updateSurvey(Survey survey);
+    public String deleteSurveyById(Integer surveyId);
+
+
+}
--- osp/src/main/java/com/lowes3/osp/service/SurveyServiceImpl.java
+++ osp/src/main/java/com/lowes3/osp/service/SurveyServiceImpl.java
@@ -1,6 +1,42 @@
 package com.lowes3.osp.service;
 
+import com.lowes3.osp.dao.SurveyRepository;
+import com.lowes3.osp.entity.Survey;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class SurveyServiceImpl implements SurveyService{}
+public class SurveyServiceImpl implements SurveyService {
+    @Autowired
+    private SurveyRepository surveyRepository;
+
+    @Override
+    public Survey saveSurvey(Survey survey) {
+        return surveyRepository.save(survey);
+    }
+
+    @Override
+    public Survey getSurveyById(Integer surveyId) {
+        Optional<Survey> survey = surveyRepository.findById(surveyId);
+        if (survey.isPresent()) return survey.get();
+        else return null;
+    }
+
+    @Override
+    public Survey updateSurvey(Survey survey) {
+        if(surveyRepository.existsById(survey.getSurveyId())) return surveyRepository.save(survey);
+        else return null;
+    }
+
+    @Override
+    public String deleteSurveyById(Integer surveyId) {
+        Optional<Survey> survey = surveyRepository.findById(surveyId);
+        if (survey.isPresent()){
+            surveyRepository.deleteById(surveyId);
+            return "Deleted the Survey.";
+        }
+        else return "Could not find Survey by Id.";
+    }
+}
