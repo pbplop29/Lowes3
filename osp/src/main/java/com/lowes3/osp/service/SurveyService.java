6d05c7e55219565b54f3a7e1684e351519b0eba7
pbplop29 <pbplop@gmail.com>
2023-04-19 00:34:26 +0530

    created and added service and service implementation

---
+++ osp/src/main/java/com/lowes3/osp/service/SurveyService.java
@@ -0,0 +1,6 @@
+package com.lowes3.osp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SurveyService {}
---
+++ osp/src/main/java/com/lowes3/osp/service/SurveyServiceImpl.java
@@ -0,0 +1,6 @@
+package com.lowes3.osp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SurveyServiceImpl implements SurveyService{}
