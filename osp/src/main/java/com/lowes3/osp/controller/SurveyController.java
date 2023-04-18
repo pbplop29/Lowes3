385833b727d21356348a2e3ea13ef1c58f5365ba
pbplop29 <pbplop@gmail.com>
2023-04-19 01:24:15 +0530

    Completed CRUD operations on survey

--- osp/src/main/java/com/lowes3/osp/controller/SurveyController.java
+++ osp/src/main/java/com/lowes3/osp/controller/SurveyController.java
@@ -10,22 +10,26 @@ public class SurveyController {
     @Autowired
     private SurveyService surveyService;
 
+    //http://localhost:8080/saveSurvey
     @PostMapping("/saveSurvey")
     public Survey saveSurvey(@RequestBody Survey survey) {
         Survey survey1 = surveyService.saveSurvey(survey);
         return survey1;
     }
 
+    //http://localhost:8080/getSurvey/1
     @GetMapping("/getSurvey/{surveyId}")
     public Survey getSurveyById(@PathVariable("surveyId") Integer surveyId) {
         return surveyService.getSurveyById(surveyId);
     }
 
+    //http://localhost:8080/updateSurvey
     @PutMapping("/updateSurvey")
     public Survey updateSurvey(@RequestBody Survey survey){
         return surveyService.updateSurvey(survey);
     }
 
+    //http://localhost:8080/deleteSurvey/1
     @GetMapping("/deleteSurvey")
     public String deleteSurveyByID(@PathVariable("surveyId") Integer surveyId){
         String checkIfDeleted = surveyService.deleteSurveyById(surveyId);
