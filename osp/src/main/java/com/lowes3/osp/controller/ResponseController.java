540fd9a6a42753eec4c7fb6fb356380c5bf5708b
NSHR110301 <69975222+NSHR110301@users.noreply.github.com>
2023-04-21 18:00:20 +0530

    Add files via upload

---
+++ osp/src/main/java/com/lowes3/osp/controller/ResponseController.java
@@ -0,0 +1,50 @@
+package com.lowes3.osp.controller;
+import org.springframework.stereotype.Controller;
+
+
+
+import com.lowes3.osp.entity.Response;
+import com.lowes3.osp.service.ResponseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+package com.lowes3.osp.controller;
+
+import com.lowes3.osp.entity.Question;
+import com.lowes3.osp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class ResponseController {
+    @Autowired
+    private ResponseService responseService;
+
+    //Create or save or add
+    //http://localhost:8080/saveResponse
+    @PostMapping("/saveResponse")
+    public Response saveResponse (@RequestBody Response response) {
+    	Response response1 = responseService.saveResponse(response);
+        return response1;
+    }
+
+    //http://localhost:8080/getResponse /1
+    @GetMapping("/getResponse /{userId}")
+    public Response getResponseById(@PathVariable("userId") Integer userId) {
+        return responseService.getResponseById(userId);
+    }
+
+    //http://localhost:8080/updateQuestion
+    @PutMapping("/updateResponse ")
+    public Response  updateResponse (@RequestBody Response response ){
+        return responseService.updateResponse(response);
+    }
+
+    //http://localhost:8080/deleteResponse/1
+    @DeleteMapping("/deleteResponse/{userId}")
+    public String deleteResponseByID(@PathVariable("userId") Integer userId){
+        String checkIfDeleted = responseService.deleteResponseById(userId);
+        return checkIfDeleted;
+    }
+}
