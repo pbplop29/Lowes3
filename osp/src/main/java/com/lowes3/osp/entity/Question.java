09037fb9de9752c5899718790c4301d6523b6320
Antara Chakrabarti <56504745+ElixerofCoding144@users.noreply.github.com>
2023-04-23 20:15:08 +0530

    ManyToOne -> Survey ; OneToMany ->List<Response>

--- osp/src/main/java/com/lowes3/osp/entity/Question.java
+++ osp/src/main/java/com/lowes3/osp/entity/Question.java
@@ -14,7 +14,19 @@ import lombok.*;
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "question_Id")
     private Integer questionId;
+    @Column(name = "question_Type")
     private String questionType;
+    @Column(name = "question_Description")
     private String questionDescription;
+    
+    //Many To One relationship with survey
+    @ManyToOne
+    @JoinColumn(name = "survey_Id")
+    private Survey survey;
+    
+    //One to Many relationship with Response
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List <Response> response;
 }
