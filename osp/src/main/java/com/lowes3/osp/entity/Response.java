73e9e7fc85b348efb944f242baded59d3a7a97a2
Antara Chakrabarti <56504745+ElixerofCoding144@users.noreply.github.com>
2023-04-23 20:17:30 +0530

    ManyToOne ->Question ;  ManyToOne ->User

--- osp/src/main/java/com/lowes3/osp/entity/Response.java
+++ osp/src/main/java/com/lowes3/osp/entity/Response.java
@@ -14,7 +14,19 @@ import lombok.*;
 public class Response {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "response_Id")
     private Integer responseId;
+    @Column(name = "response_Details")    
     private String responseDetails;
+
+    //Many To One relationship with survey
+    @ManyToOne
+    @JoinColumn(name = "question_Id")
+    private Question question;
+    
+  //Many To One relationship with survey
+    @ManyToOne
+    @JoinColumn(name = "user_Id")
+    private User user;
 
 }
