54dcabe32286c473ab06b1aa19b51f5f543fc18b
pbplop29 <pbplop@gmail.com>
2023-05-10 10:25:29 +0530

    created a mapping based routing system for easy modifications to routes

--- ws/osp/src/App.js
+++ ws/osp/src/App.js
@@ -4,14 +4,20 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import { Routes, Route } from "react-router-dom";
 
+const routes = [
+  { path: "/", name: "Home", Component: Home },
+  { path: "/about", name: "About", Component: About },
+];
+
 function App() {
   return (
     <div className="App">
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        {routes.map(({ path, Component }) => (
+          <Route key="name" path={path} element={<Component />}></Route>
+        ))}
       </Routes>
     </div>
   );
