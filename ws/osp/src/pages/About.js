bf6bbfb11910ab37d92fd12dbae9a6c803bda52b
pbplop29 <pbplop@gmail.com>
2023-05-24 18:26:02 +0530

    changed stuff in the about page css layout and all

--- ws/osp/src/pages/About.js
+++ ws/osp/src/pages/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/About.css";
-import example from "../assets/leap-logo.jpg";
+import leapLogo from "../assets/leap-logo.jpg";
 
 const teamMembers = [
   {
@@ -8,26 +8,30 @@ const teamMembers = [
     name: "Antara Chakrabarti",
     email: "mailantara6@gmail.com",
     college: "Mody University of Science and Technology",
+    emoji: "😃",
   },
   {
     id: 2,
     name: "Anushree Deshpande",
     email: "anushree.deshpande@cumminscollege.in",
     college: "CCOEW pune",
+    emoji: "",
   },
 
   {
     id: 3,
-    name: "Biplov",
+    name: "Biplov Pokhrel",
     email: "biplov.nitrkl@gmail.com",
     college: "NIT Rourkela",
+    emoji: "🐣",
   },
 
   {
     id: 4,
-    name: "Chetna",
+    name: "Chetna Ahir",
     email: "chetnaahir555@gmail.com",
     college: "Mody University of Science and Technology",
+    emoji: "😠",
   },
 
   {
@@ -35,6 +39,7 @@ const teamMembers = [
     name: "Jaspriya Gujral",
     email: "jaspriya12@gmail.com",
     college: "Mody University of Science and Technology",
+    emoji: "😆",
   },
 
   {
@@ -42,6 +47,7 @@ const teamMembers = [
     name: "Siddharth Das",
     email: "sdas200015@gmail.com",
     college: "NIT Silchar",
+    emoji: "😴",
   },
 
   {
@@ -49,6 +55,7 @@ const teamMembers = [
     name: "Sudha Chintake",
     email: "sudha.chintake@cumminscollege.in",
     college: "CCOEW Pune",
+    emoji: "😁",
   },
 
   {
@@ -56,46 +63,51 @@ const teamMembers = [
     name: "Utkarsh Mishra",
     email: "pranshuu17@gmail.com",
     college: "NIE Mysore",
+    emoji: "🤓",
   },
   // Add more team members here...
 ];
 
 const About = () => {
   return (
-    <div className="about">
-      <h2>About Us</h2>
-      <div className="container">
-        <img src={example} alt="Example" className="image" />
-        <text className="text">
-          Lowe’s Engineering Accelerator Program The Lowe’s Engineering
-          Acceleration Program (LEAP) seeks to disrupt and transform the digital
-          landscape, thereby powering Lowe’s towards a new era. We have based
-          LEAP on the concept of muscle memory. At LEAP, we believe that working
-          on a new skill repeatedly in a conducive environment and applying it
-          to real-world problems is the key to gain expertise on the skill. LEAP
-          enables the teams to come with their real-world problems for a period,
-          get coached to acquire relevant skills in Product, Agile, DevOps, and
-          Engineering domains. The team continues to deliver value while
-          learning and improving skills.
-        </text>
-      </div>
-
-      <h2>Team</h2>
-      <div className="team-members">
-        {teamMembers.map((member) => (
-          <div className="member-card" key={member.id}>
-            <h3>{member.name}</h3>
-            <p>{member.role}</p>
-            <p>Email: {member.email}</p>
-            <p>College: {member.college}</p>
-            <p></p>
+    <>
+      <div className="about_description_container">
+        <div className="about_us_title">About Us</div>
+        <div className="description_and_logo">
+          <img src={leapLogo} alt="Leap Logo" className="leap_logo" />
+          <div className="description">
+            The Lowe’s Engineering Acceleration Program (LEAP) seeks to disrupt
+            and transform the digital landscape, thereby powering Lowe’s towards
+            a new era. We have based LEAP on the concept of muscle memory.
+            <br /> <br /> At LEAP, we believe that working on a new skill
+            repeatedly in a conducive environment and applying it to real-world
+            problems is the key to gain expertise on the skill. <br /> <br />{" "}
+            LEAP enables the teams to come with their real-world problems for a
+            period, get coached to acquire relevant skills in Product, Agile,
+            DevOps, and Engineering domains. The team continues to deliver value
+            while learning and improving skills.
           </div>
-        ))}
+        </div>
       </div>
 
-      <div className="Usecase-3">
-        {" "}
-        <h2>Task : Usecase-3</h2>{" "}
+      <div className="team_members_container">
+        <div className="about_us_title">The Team</div>
+        <div className="team_members">
+          {teamMembers.map((member) => (
+            <div className="member_card" key={member.id}>
+              <div style={{ "font-size": "20px", "font-weight": "bolder" }}>
+                {member.name}
+              </div>{" "}
+              <div className="member_emoji">{member.emoji}</div>
+              {member.role} <br />
+              {member.email} <br />
+              {member.college}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="task_description_container">
+        <div className="about_us_title">The Task</div>
         <p> Post training assesment to test the learnings of the trainees.</p>
         <h4> Front-end:</h4>
         <ul>
@@ -113,7 +125,7 @@ const About = () => {
           <li>Server-side rendering</li>
         </ul>
       </div>
-    </div>
+    </>
   );
 };
 
