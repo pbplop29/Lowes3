d4b930c1a1ea48ea9f747b0a0d86ddf8cafe3cce
17mishra <utkarsh17072000@gmail.com>
2023-05-24 13:37:08 +0530

    about.js and about.css page up and running

--- ws/osp/src/pages/About.js
+++ ws/osp/src/pages/About.js
@@ -6,49 +6,49 @@ const teamMembers = [
   {
     id: 1,
     name: 'Antara Chakrabarti',
-    email: 'john.doe@example.com',
-    college: ''
+    email: 'mailantara6@gmail.com',
+    college: 'Mody University of Science and Technology'
   },
   {
     id: 2,
     name: 'Anushree Deshpande',
-    email: 'jane.smith@example.com',
-    college: ''
+    email: 'anushree.deshpande@cumminscollege.in',
+    college: 'CCOEW pune'
   },
 
   {
     id: 3,
     name: 'Biplov',
-    email: 'jane.smith@example.com',
-    college: ''
+    email: 'biplov.nitrkl@gmail.com',
+    college: 'NIT Rourkela'
   },
 
   {
     id: 4,
     name: 'Chetna',
-    email: 'jane.smith@example.com',
-    college: ''
+    email: 'chetnaahir555@gmail.com',
+    college: 'Mody University of Science and Technology'
   },
 
   {
     id: 5,
     name: 'Jaspriya Gujral',
-    email: 'jane.smith@example.com',
-    college: ''
+    email: 'jaspriya12@gmail.com',
+    college: 'Mody University of Science and Technology'
   },
 
   {
     id: 6,
     name: 'Siddharth Das',
-    email: 'jane.smith@example.com',
-    college: ''
+    email: 'sdas200015@gmail.com',
+    college: 'NIT Silchar'
   },
 
   {
     id: 7,
     name: 'Sudha Chintake',
-    email: 'jane.smith@example.com',
-    college: ''
+    email: 'sudha.chintake@cumminscollege.in',
+    college: 'CCOEW Pune'
   },
 
   {
@@ -70,9 +70,10 @@ const About = () => {
       <text className="text">Lowe’s Engineering Accelerator Program
 The Lowe’s Engineering Acceleration Program (LEAP) seeks to disrupt and transform the digital landscape, thereby powering Lowe’s towards a new era. We have based LEAP on the concept of muscle memory. At LEAP, we believe that working on a new skill repeatedly in a conducive environment and applying it to real-world problems is the key to gain expertise on the skill. LEAP enables the teams to come with their real-world problems for a period, get coached to acquire relevant skills in Product, Agile, DevOps, and Engineering domains. The team continues to deliver value while learning and improving skills.</text>
 </div>
-      <div className="Usecase-3"> <p> Usecase-3 is the task for post training assesment to test the learnings of the trainees.
-        </p> </div>
+      
+        <h2>Team</h2>
       <div className="team-members">
+      
         {teamMembers.map(member => (
           <div className="member-card" key={member.id}>
             <h3>{member.name}</h3>
@@ -83,6 +84,26 @@ The Lowe’s Engineering Acceleration Program (LEAP) seeks to disrupt and transf
           </div>
         ))}
       </div>
+
+      <div className="Usecase-3"> <h2>Task : Usecase-3</h2> <p> Post training assesment to test the learnings of the trainees.
+        </p>
+        
+        <h4> Front-end:</h4>
+        <ul>
+          <li>HTML, CSS, and JavaScript for building user interfaces</li>
+          <li>JavaScript frameworks such as React for building web applications</li>
+        </ul>
+
+        <h4> Back-end:</h4>
+        <ul>
+          <li>Programming language Java</li>
+          <li>Spring Boot and spring framework</li>
+          <li>H2 Database</li>
+          <li>RESTful API design</li>
+          <li>Server-side rendering</li>
+        </ul>
+
+        </div>
     </div>
   );
 };
